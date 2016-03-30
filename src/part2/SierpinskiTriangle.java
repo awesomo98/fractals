67a8dd7e48be7f31c7bf69aa286872ed8c6c8a6e
Bilal Reza <rezasb@s.dcsdk12.org>
2016-03-30 10:49:34 -0600

    Git sierpinskied

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -1,6 +1,9 @@
 package part2;
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 import draw.StdDraw;
 
 
@@ -23,12 +26,14 @@ public class SierpinskiTriangle {
    
 	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
 	   drawTriangle(a, b, c, Color.WHITE);
-	   if (n <= 0) {
-		   Point2D.Double newA = Point2D.Double(((a.x - c.x) / 2), ((a.y - c.y) / 2));
-		   Point2D.Double newB = Point2D.Double()
-		   Point2D.Double newC = Point2D.Double()
-//		   return sierpinski(((b.x - a.x) / 2), ((c.x - a.x) / 2), ((a.x - c.x) / 2));
-	   }
+	   if (n <= 0) return;
+		   Point2D.Double newA = new Point2D.Double(((b.x + a.x) / 2), ((b.y + a.y) / 2));
+		   Point2D.Double newB = new Point2D.Double(((b.x + c.x) / 2), ((b.y + c.y) / 2));
+		   Point2D.Double newC = new Point2D.Double(((a.x + c.x) / 2), ((a.y + c.y) / 2));
+//		   return sierpinski(((b.x + a.x) / 2), ((c.x + a.x) / 2), ((a.x + c.x) / 2));
+//		   drawTriangle(newA, newB, newC, Color.BLACK);
+//		   return sierpinski(newA, newB, newC);
+		   return;
 		
     }
    
