e754a42a5175903962f526b00ce09f4e8b6b7545
Bilal Reza <rezasb@s.dcsdk12.org>
2016-04-04 10:14:39 -0600

    Making progress on HTree

--- src/part3/HTree.java
+++ src/part3/HTree.java
@@ -1,5 +1,7 @@
 package part3;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 import draw.StdDraw;
 import java.awt.Color;
 
@@ -12,18 +14,46 @@ public class HTree {
 	}
 
 
-	public HTree() {
-		if (n <= 0) {
-			
-		}
+	public HTree(Double center, int n) {
+		if (n <= 0) return;
+		StdDraw.setYscale(0, 150);
+		StdDraw.setXscale(0, 150);
+		
+		center = new Point2D.Double();
+		
+		StdDraw.clear(StdDraw.GRAY);
+		
+		H h = new H(center, center.x);
+		
+		draw();
+		
+		
 		
 	}
 	
 	public void draw(Point2D.Double a, Point2D.Double b, Point2D.Double c, Point2D.Double d) {
-		double[] x = {a.x, b.x, c.x};
-		double[] y = {a.y, b.y, c.y};
-		StdDraw.setPenColor(color);
-		StdDraw.filledPolygon(x, y);
+		recurTree(0, 0, 0).draw();
+	}
+	
+	public void recurTree(Double center, int Center, int n) {
+		
+		H h = new H(center, 155);
+		Point2D.Double topRight = h .getTopRight();
+		Point2D.Double topLeft = h.getTopLeft();
+		Point2D.Double bottomRight = h.getBottomRight();
+		Point2D.Double bottomLeft = h.getBottomLeft();
+		
+		n = h.getSize() / 2;
+		n--;
+		
+		if (n <= 0) {
+			return;
+		} else {
+			recurTree(topRight, n - 1, next);
+			recurTree(topLeft, n - 1, next);
+			recurTree(bottomRight, n - 1, next);
+			recurTree(bottomLeft, n - 1, next);
+		}
 	}
 	
 }
