29a5eb8ba0ed1cd8d8e829c5799f0ebdcab3d7fc
Bilal Reza <rezasb@s.dcsdk12.org>
2016-04-06 11:03:43 -0600

    Done with HTree

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -1,30 +1,36 @@
 package part2;
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 
-import draw.StdDraw;
+import draw.UltraDraw;
 
 
-public class SierpinskiTriangle { 
+public class SierpinskiTriangle implements MouseListener, MouseMotionListener { 
    
-	private Point2D.Double top, right, left;
+	private static Point2D.Double top;
+	private static Point2D.Double right;
+	private static Point2D.Double left;
 	private Color background = new Color(34, 129, 34);
-	private Color foreground = StdDraw.YELLOW;
+	private Color foreground = UltraDraw.YELLOW;
 	
 	public SierpinskiTriangle() {
-		StdDraw.setXscale(0, 150); 
-		StdDraw.setYscale(75, 150); 
+		UltraDraw.addMouseListener(this);
+		UltraDraw.setXscale(0, 150); 
+		UltraDraw.setYscale(75, 150); 
 		
 		top = new Point2D.Double(75, 150);
 		right = new Point2D.Double(150, 75);
 		left = new Point2D.Double(0, 75);
 		 
-		StdDraw.clear(StdDraw.GRAY); 
+		UltraDraw.clear(UltraDraw.GRAY); 
 	}
    
-	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
+	public static void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
 	   
 	   Point2D.Double newA = new Point2D.Double(((a.x + c.x) / 2), ((a.y + c.y) / 2));
 	   Point2D.Double newB = new Point2D.Double(((a.x + b.x) / 2), ((a.y + b.y) / 2));
@@ -48,21 +54,65 @@ public class SierpinskiTriangle {
 	   }
     }
    
-	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
+	public static void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
 	   double[] x = {a.x, b.x, c.x};
 	   double[] y = {a.y, b.y, c.y};
-	   StdDraw.setPenColor(color);
-	   StdDraw.filledPolygon(x, y);
+	   UltraDraw.setPenColor(color);
+	   UltraDraw.filledPolygon(x, y);
 	}
 	
-	public void draw(int n) {
+	public static void draw(int n) {
 		sierpinski(top, right, left, n);
 	}
 	
 	
+	public static int recursionFactor = 2;
+	
 	
 	public static void main(String[] args) { 
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(8);
+		triangle.draw(recursionFactor);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println(e);
+		recursionFactor ++;
+		SierpinskiTriangle.draw(recursionFactor);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/part3/HTree.java
+++ src/part3/HTree.java
@@ -2,59 +2,90 @@ package part3;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 
-import draw.StdDraw;
+import draw.UltraDraw;
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 
-public class HTree {
+public class HTree implements KeyListener {
 	
 	public static void main(String[] args) {
-		HTree htree = new HTree(null, 0);
-		htree.draw(3);
+		HTree htree = new HTree();
+		htree.draw(4);
 	}
 
 
-	public HTree(Double center, int n) {
-		if (n <= 0) return;
-		StdDraw.setYscale(0, 150);
-		StdDraw.setXscale(0, 150);
-		
-		center = new Point2D.Double();
-		
-		StdDraw.clear(StdDraw.GRAY);
-		
-		H h = new H(center, center.x);
-		
-		draw();
-		
-		
-		
+	public HTree() {
+		UltraDraw.setYscale(0, 150);
+		UltraDraw.setXscale(0, 150);
+		UltraDraw.clear(UltraDraw.GRAY);
+		UltraDraw.setPenColor(UltraDraw.WHITE);
 	}
 	
-	public void draw(Point2D.Double a, Point2D.Double b, Point2D.Double c, Point2D.Double d) {
-		recurTree(0, 0, 0).draw();
+	public void draw(int n) {
+		tree(new Point2D.Double(150 / 2, 150 / 2), 75, n);
 	}
 	
-	public void recurTree(Double center, double d, double newSize2) {
-		double newSize = H.getSize();
+	public void tree (Point2D.Double center, double radius, int n){
+		if (n < 0) return;
+		H h = new H(center, radius);
+		h.draw();
+		H topLeft = new H(h.getTopLeft(), h.getSize());
+		H topRight = new H(h.getTopLeft(), h.getSize());
+		H bottomLeft = new H(h.getTopLeft(), h.getSize());
+		H bottomRight = new H(h.getTopLeft(), h.getSize());
 		
-		H h = new H(center, 155);
-		Point2D.Double topRight = h .getTopRight();
-		Point2D.Double topLeft = h.getTopLeft();
-		Point2D.Double bottomRight = h.getBottomRight();
-		Point2D.Double bottomLeft = h.getBottomLeft();
+		tree(h.getTopLeft(), radius / 2, (n-1));
+		tree(h.getTopRight(), radius / 2,  (n-1));
+		tree(h.getBottomLeft(), radius / 2, (n-1));
+		tree(h.getBottomRight(), radius / 2, (n-1));
+	}
+	
+//	public void recurTree(Double center, double d, double newSize2) {
+//		double newSize = H.getSize();
+//		
+//		H h = new H(center, 155);
+//		Point2D.Double topRight = h .getTopRight();
+//		Point2D.Double topLeft = h.getTopLeft();
+//		Point2D.Double bottomRight = h.getBottomRight();
+//		Point2D.Double bottomLeft = h.getBottomLeft();
+//		
+//		newSize2 = (int) (h.getSize() / 2);
+//		newSize2--;
+//		
+//		if (newSize2 <= 0) {
+//			return;
+//		} else {
+//			recurTree(topRight, newSize2 - 1, newSize);
+//			recurTree(topLeft, newSize2 - 1, newSize);
+//			recurTree(bottomRight, newSize2 - 1, newSize);
+//			recurTree(bottomLeft, newSize2 - 1, newSize);
+//		}
+//	}
+	
+	public static int recursionFactor = 2;
+
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		char keychar = arg0.getKeyChar();
+		System.out.println(arg0);
+//		HTree.draw(recursionFactor);
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
-		newSize2 = (int) (h.getSize() / 2);
-		newSize2--;
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
-		if (newSize2 <= 0) {
-			return;
-		} else {
-			recurTree(topRight, newSize2 - 1, newSize);
-			recurTree(topLeft, newSize2 - 1, newSize);
-			recurTree(bottomRight, newSize2 - 1, newSize);
-			recurTree(bottomLeft, newSize2 - 1, newSize);
-		}
 	}
 	
 }
