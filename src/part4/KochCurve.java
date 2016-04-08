e97b8284b68bd878aba061f169ae56aca817fd28
Bilal Reza <rezasb@s.dcsdk12.org>
2016-04-08 10:03:58 -0600

    Started Koch

--- src/part4/KochCurve.java
+++ src/part4/KochCurve.java
@@ -1,10 +1,37 @@
 package part4;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 import draw.StdDraw;
+import draw.UltraDraw;
 
 
 public class KochCurve {
-
-	// TODO: implement
+	public static void main(String[] args) {
+		KochCurve curve = new KochCurve();
+		curve.draw(2);
+	}
 	
+	public KochCurve() {
+		UltraDraw.setYscale(0, 150);
+		UltraDraw.setXscale(0, 150);
+		UltraDraw.clear(UltraDraw.GRAY);
+		UltraDraw.setPenColor(UltraDraw.WHITE);
+		
+		Double startPoint = new Point2D.Double(25, 75);
+		Double midPoint = new Point2D.Double(75, 75);
+	}
+	
+	public void drawCurve(Point2D.Double a, Point2D.Double b, Point2D.Double c) {
+		double[] x = {a.x, b.x, c.x};
+		double[] y = {a.x, b.x, c.x};
+	}
+	
+	public void draw(int n) {
+		curve(left, right, top);
+	}
+	
+	public void curve(Point2D.Double left, Point2D.Double right, Point2D.Double top) {
+		
+	}
 }
