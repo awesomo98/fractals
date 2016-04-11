e20d6a0f118eb700dd012667029f1ee66a01f6b8
awesomo98 <bilalreza@gmail.com>
2016-04-10 22:10:27 -0600

    Almost there

--- src/part4/KochSnowflake.java
+++ src/part4/KochSnowflake.java
@@ -1,10 +1,23 @@
 package part4;
 import java.awt.geom.Point2D;
-import draw.StdDraw;
+import draw.UltraDraw;
 
 
 public class KochSnowflake {
-
-	// TODO: implement
-
+	public static void main(String[] args) {
+		KochSnowflake help = new KochSnowflake();
+		help.draw(2);
+	}
+	
+  public void draw(int n) {
+	  KochCurve one = new KochCurve();
+	  KochCurve two = new KochCurve();
+	  KochCurve three = new KochCurve();
+	  Point2D.Double left = new Point2D.Double(12.0,256.0);
+	  Point2D.Double right = new Point2D.Double(500.0,256.0);
+	  Point2D.Double top = new Point2D.Double(256,12.0);
+	  one.draw(n,left,top,true);
+	  two.draw(n,top, right, true);
+	  three.draw(n, left, right, false);
+	  }
 }
